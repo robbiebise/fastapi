65552d600d687fbf7de24c1e85af9581b0d1ba87
Robbie Bise <theil.bise@sonarsource.com>
2023-01-16 02:56:01 -0600

    asdf

--- fastapi/routing.py
+++ fastapi/routing.py
@@ -314,6 +314,7 @@ class APIWebSocketRoute(routing.WebSocketRoute):
 
     def matches(self, scope: Scope) -> Tuple[Match, Scope]:
         match, child_scope = super().matches(scope)
+        print("hello")
         if match != Match.NONE:
             child_scope["route"] = self
         return match, child_scope
